b2d7c7937f3e0864a35f2ff6fe51f6e8a9709564
RB <82284311+Roman123-game@users.noreply.github.com>
2024-02-17 16:50:05 +0200

    build(deps): update App.js, package-lock.json and package.json

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>  new App appointment</Text>
+      <Image style={styles.mainImage} source={require('./assets/Frame2.png')} />
+      <Button title="tap me"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderRadius: '50px',
+    maxWidth: 320,
+    margin: 'auto',
+    minHeight:680,
+    backgroundColor: '#f6eee3'
+
+
   },
+  mainImage:{
+    borderColor: 'black',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderRadius: '15px',
+    backgroundColor: '#e5cbba',
+
+  }
 });
